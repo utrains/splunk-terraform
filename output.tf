2a31053d9640f081a51c44166c21fb6064ef70b6
Queenpamsy <azanfouetpamela@gmail.com>
2023-10-12 20:08:00 +0100

    removing usuable data

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 # print the url of the splunk server and the splunk forwader
-#output "ssh_splunk_forwader_command" {
-#  value     = join ("", ["ssh -i splunkkey.pem ec2-user@", aws_instance.splunk-forwarder.public_dns])
-#}
+output "ssh_splunk_forwader_command" {
+  value     = join ("", ["ssh -i splunkkey.pem ec2-user@", aws_instance.splunk-forwarder.public_dns])
+}
 
 output "Connexion_link_for_the_splunk_server" {
   value     = join ("", ["http://", aws_instance.splunk-server.public_ip, ":", "8000"])
 }
-output "Connexion_link_for_the_splunk_forwarder" {
-  value     = join ("", ["http://", aws_instance.splunk-forwarder.public_ip, ":", "9997"])
-}
+#output "Connexion_link_for_the_splunk_forwarder" {
+#  value     = join ("", ["http://", aws_instance.splunk-forwarder.public_ip, ":", "9997"])
+#}
 
 output "ssh_splunk_server_command" {
   value     = join ("", ["ssh -i splunkkey.pem ec2-user@", aws_instance.splunk-server.public_dns])
